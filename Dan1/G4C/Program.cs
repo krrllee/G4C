d5f8147bf27ce3a2a8ce1d05af19ac5330de6619
krrllee <krleposkok12@gmail.com>
2023-09-11 14:02:49 +0200

    dan 1 resenja

--- Dan1/G4C/Program.cs
+++ Dan1/G4C/Program.cs
@@ -1,1 +1,118 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+//zadatak 1
+void Calculator()
+{
+    
+   while(true)
+    {
+        var input = int.Parse(Console.ReadLine());
+        if (input.Equals('x')) {
+            return;
+          } 
+
+        Console.WriteLine(input * input);
+    }
+}
+
+//zadatak 2
+void Fibonaci(int n)
+{
+    int first = 0;
+    int second = 1;
+    int result = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        result = first + second;
+        first = second;
+        second = result;
+        Console.WriteLine(result);
+    }
+}
+
+//zadatak 3
+void Deljivost()
+{
+    var input = int.Parse(Console.ReadLine());
+    List<int> numbers = Enumerable.Range(1, 100).ToList();
+    List<int> divided = numbers.Where(x=> x % input == 0).ToList();
+
+    foreach(int item in divided) {
+        Console.WriteLine(item);
+    }
+}
+
+//zadatak 4
+Osoba osoba1 = new Osoba();
+osoba1.Zadatak6();
+
+public enum Pol
+    {
+        MUSKO,
+        ZENSKO
+    }
+
+    public class Osoba
+    {
+        string ime;
+        int starost;
+        Pol pol;
+
+
+        public string Ime { get => ime; set => ime = value; }
+        public int Starost { get => starost; set => starost = value; }
+        public Pol Pol { get => pol; set => pol = value; }
+
+    //zadatak 5
+    public void Zadatak5()
+        {
+            List<Osoba> osobe = new List<Osoba>
+            {
+                new Osoba { Ime = "Marko", Starost = 30, Pol = Pol.MUSKO },
+                new Osoba { Ime = "Ana", Starost = 20, Pol = Pol.ZENSKO }
+            };
+
+            var sorted = osobe.OrderByDescending(x => x.starost).ToList();
+
+            foreach(var item in sorted)
+            {
+                Console.WriteLine(item.Ime + " " +  item.Starost);
+            }
+        }
+
+    //zadatak 6
+    public void Zadatak6()
+    {
+        List<Osoba> osobe = new List<Osoba>
+            {
+                new Osoba { Ime = "Marko", Starost = 30, Pol = Pol.MUSKO },
+                new Osoba { Ime = "Ana", Starost = 20, Pol = Pol.ZENSKO }
+            };
+
+        var grupisaneOsobe = from osoba in osobe
+                             group osoba by osoba.Pol into grupeOsoba
+                             select new
+                             {
+                                 Pol = grupeOsoba.Key,
+                                 Osobe = grupeOsoba.Select(o => new { Ime = o.Ime, Godina = o.Starost })
+                             };
+        foreach (var grupa in grupisaneOsobe)
+        {
+            Console.WriteLine($"Pol: {grupa.Pol}");
+            foreach (var osoba in grupa.Osobe)
+            {
+                Console.WriteLine($"Ime: {osoba.Ime}, Godina: {osoba.Godina}");
+            }
+        }
+
+
+
+
+        }
+
+}
+
+
+//Calculator();
+//Fibonaci(5);
+//Deljivost();
